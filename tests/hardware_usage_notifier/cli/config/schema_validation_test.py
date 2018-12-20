afc3b016b821681e76eab284ad366c53f53d815b
Ovidiu <ovidiu.pricop@gmail.com>
2018-12-20 17:38:35 +0200

    Add more schema unit tests to cover type validation

--- tests/hardware_usage_notifier/cli/config/schema_validation_test.py
+++ tests/hardware_usage_notifier/cli/config/schema_validation_test.py
@@ -25,12 +25,34 @@ NO_THRESHOLD_COMPARATOR_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no
 NO_THRESHOLD_VALUE_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_threshold_value_config.json')
 NO_THRESHOLD_ALARM_POINTS_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_threshold_alarm_points_config.json')
 NO_THRESHOLD_CLEAR_POINTS_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_threshold_clear_points_config.json')
-NO_NOTIFIERS_MONITOR_ALARM_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_notifiers_monitor_alarm_config.json')
-NO_NOTIFIERS_MONITOR_FAILURE_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_notifiers_monitor_failure_config.json')
+NO_NOTIFIERS_MONITOR_ALARM_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY,
+                                                      'no_notifiers_monitor_alarm_config.json')
+NO_NOTIFIERS_MONITOR_FAILURE_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY,
+                                                        'no_notifiers_monitor_failure_config.json')
 NO_MONITOR_ALARM_NAME_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_monitor_alarm_name_config.json')
-NO_MONITOR_ALARM_CONFIGURATION_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_monitor_alarm_configuration_config.json')
+NO_MONITOR_ALARM_CONFIGURATION_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY,
+                                                          'no_monitor_alarm_configuration_config.json')
 NO_MONITOR_FAILURE_NAME_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_monitor_failure_name_config.json')
-NO_MONITOR_FAILURE_CONFIGURATION_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'no_monitor_failure_configuration_config.json')
+NO_MONITOR_FAILURE_CONFIGURATION_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY,
+                                                            'no_monitor_failure_configuration_config.json')
+METRIC_NOT_OBJECT_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'metric_not_object_config.json')
+THRESHOLD_NOT_OBJECT_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'threshold_not_object_config.json')
+MONITOR_NAME_NOT_STRING_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'monitor_name_not_string_config.json')
+MONITOR_DESCRIPTION_NOT_STRING_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'monitor_description_not_string_config.json')
+METRIC_NAME_NOT_STRING_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'metric_name_not_string_config.json')
+METRIC_CONFIGURATION_NOT_OBJECT_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'metric_configuration_not_object_config.json')
+THRESHOLD_COMPARATOR_NOT_STRING_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'threshold_comparator_not_string_config.json')
+THRESHOLD_VALUE_NOT_STRING_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'threshold_value_not_string_config.json')
+THRESHOLD_ALARM_POINTS_NOT_NUMBER_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'threshold_alarm_points_not_number_config.json')
+THRESHOLD_CLEAR_POINTS_NOT_NUMBER_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'threshold_clear_points_not_number_config.json')
+MONITOR_INTERVAL_NOT_NUMBER_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'monitor_interval_not_number_config.json')
+NOTIFIERS_NOT_OBJECT_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'notifiers_not_object_config.json')
+MONITOR_ALARM_NOT_OBJECT_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'monitor_alarm_not_object_config.json')
+ALARM_NAME_NOT_STRING_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'alarm_name_not_string_config.json')
+ALARM_CONFIGURATION_NOT_OBJECT_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'alarm_configuration_not_object_config.json')
+MONITOR_FAILURE_NOT_OBJECT_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'monitor_failure_not_object_config.json')
+FAILURE_NAME_NOT_STRING_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'failure_name_not_string_config.json')
+FAILURE_CONFIGURATION_NOT_OBJECT_CONFIG_FILE = os.path.join(TEST_RESOURCES_DIRECTORY, 'failure_configuration_not_object_config.json')
 
 
 @pytest.fixture(scope='function')
@@ -228,7 +250,8 @@ def test_when_config_contains_monitor_alarm_with_no_name_then_schema_requires_na
     )
 
 
-def test_when_config_contains_monitor_alarm_with_no_configuration_then_schema_validates_successfully(config_json_schema):
+def test_when_config_contains_monitor_alarm_with_no_configuration_then_schema_validates_successfully(
+        config_json_schema):
     no_monitor_alarm_configuration_config = read_json_from_file(NO_MONITOR_ALARM_CONFIGURATION_CONFIG_FILE)
     jsonschema.validate(no_monitor_alarm_configuration_config, config_json_schema)
 
@@ -245,6 +268,226 @@ def test_when_config_contains_monitor_failure_with_no_name_then_schema_requires_
     )
 
 
-def test_when_config_contains_monitor_failure_with_no_configuration_then_schema_validates_successfully(config_json_schema):
+def test_when_config_contains_monitor_failure_with_no_configuration_then_schema_validates_successfully(
+        config_json_schema):
     no_monitor_failure_configuration_config = read_json_from_file(NO_MONITOR_FAILURE_CONFIGURATION_CONFIG_FILE)
     jsonschema.validate(no_monitor_failure_configuration_config, config_json_schema)
+
+
+def test_when_config_monitor_name_not_string_then_schema_requires_string(config_json_schema):
+    monitor_name_not_string_config = read_json_from_file(MONITOR_NAME_NOT_STRING_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(monitor_name_not_string_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'string'",
+        ['monitors', 0, 'name']
+    )
+
+
+def test_when_config_monitor_description_not_string_then_schema_requires_string(config_json_schema):
+    monitor_description_not_string_config = read_json_from_file(MONITOR_DESCRIPTION_NOT_STRING_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(monitor_description_not_string_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'string'",
+        ['monitors', 0, 'description']
+    )
+
+
+def test_when_config_metric_not_object_then_schema_requires_object(config_json_schema):
+    metric_not_object_config = read_json_from_file(METRIC_NOT_OBJECT_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(metric_not_object_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'object'",
+        ['monitors', 0, 'metric']
+    )
+
+
+def test_when_config_metric_name_not_string_then_schema_requires_string(config_json_schema):
+    metric_name_not_string_config = read_json_from_file(METRIC_NAME_NOT_STRING_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(metric_name_not_string_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'string'",
+        ['monitors', 0, 'metric', 'name']
+    )
+
+
+def test_when_config_metric_configuration_not_object_then_schema_requires_string(config_json_schema):
+    metric_configuration_not_object_config = read_json_from_file(METRIC_CONFIGURATION_NOT_OBJECT_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(metric_configuration_not_object_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'object'",
+        ['monitors', 0, 'metric', 'configuration']
+    )
+
+
+def test_when_config_threshold_not_object_then_schema_requires_object(config_json_schema):
+    threshold_not_object_config = read_json_from_file(THRESHOLD_NOT_OBJECT_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(threshold_not_object_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'object'",
+        ['monitors', 0, 'threshold']
+    )
+
+
+def test_when_config_threshold_comparator_not_string_then_schema_requires_string(config_json_schema):
+    threshold_comparator_not_string_config = read_json_from_file(THRESHOLD_COMPARATOR_NOT_STRING_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(threshold_comparator_not_string_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'string'",
+        ['monitors', 0, 'threshold', 'comparator']
+    )
+
+
+def test_when_config_threshold_value_not_string_then_schema_requires_string(config_json_schema):
+    threshold_value_not_string_config = read_json_from_file(THRESHOLD_VALUE_NOT_STRING_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(threshold_value_not_string_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'string'",
+        ['monitors', 0, 'threshold', 'value']
+    )
+
+
+def test_when_config_threshold_alarm_points_not_number_then_schema_requires_number(config_json_schema):
+    threshold_alarm_points_not_number_config = read_json_from_file(THRESHOLD_ALARM_POINTS_NOT_NUMBER_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(threshold_alarm_points_not_number_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'number'",
+        ['monitors', 0, 'threshold', 'alarm_points']
+    )
+
+
+def test_when_config_threshold_clear_points_not_number_then_schema_requires_number(config_json_schema):
+    threshold_clear_points_not_number_config = read_json_from_file(THRESHOLD_CLEAR_POINTS_NOT_NUMBER_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(threshold_clear_points_not_number_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'number'",
+        ['monitors', 0, 'threshold', 'clear_points']
+    )
+
+
+def test_when_config_monitor_interval_not_number_then_schema_requires_number(config_json_schema):
+    monitor_interval_not_number_config = read_json_from_file(MONITOR_INTERVAL_NOT_NUMBER_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(monitor_interval_not_number_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'number'",
+        ['monitors', 0, 'interval']
+    )
+
+
+def test_when_config_notifiers_not_object_then_schema_requires_object(config_json_schema):
+    notifiers_not_object_config = read_json_from_file(NOTIFIERS_NOT_OBJECT_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(notifiers_not_object_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'object'",
+        ['monitors', 0, 'notifiers']
+    )
+
+
+def test_when_config_monitor_alarm_not_object_then_schema_requires_object(config_json_schema):
+    monitor_alarm_not_object_config = read_json_from_file(MONITOR_ALARM_NOT_OBJECT_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(monitor_alarm_not_object_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'object'",
+        ['monitors', 0, 'notifiers', 'monitor_alarm']
+    )
+
+
+def test_when_config_alarm_name_not_string_then_schema_requires_string(config_json_schema):
+    alarm_name_not_string_config = read_json_from_file(ALARM_NAME_NOT_STRING_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(alarm_name_not_string_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'string'",
+        ['monitors', 0, 'notifiers', 'monitor_alarm', 'name']
+    )
+
+
+def test_when_config_alarm_configuration_not_object_then_schema_requires_object(config_json_schema):
+    alarm_configuration_not_object_config = read_json_from_file(ALARM_CONFIGURATION_NOT_OBJECT_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(alarm_configuration_not_object_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'object'",
+        ['monitors', 0, 'notifiers', 'monitor_alarm', 'configuration']
+    )
+
+
+def test_when_config_monitor_failure_not_object_then_schema_requires_object(config_json_schema):
+    monitor_failure_not_object_config = read_json_from_file(MONITOR_FAILURE_NOT_OBJECT_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(monitor_failure_not_object_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'object'",
+        ['monitors', 0, 'notifiers', 'monitor_failure']
+    )
+
+
+def test_when_config_alarm_failure_not_string_then_schema_requires_string(config_json_schema):
+    alarm_failure_not_string_config = read_json_from_file(FAILURE_NAME_NOT_STRING_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(alarm_failure_not_string_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'string'",
+        ['monitors', 0, 'notifiers', 'monitor_failure', 'name']
+    )
+
+
+def test_when_config_failure_configuration_not_object_then_schema_requires_object(config_json_schema):
+    failure_configuration_not_object_config = read_json_from_file(FAILURE_CONFIGURATION_NOT_OBJECT_CONFIG_FILE)
+
+    exception_validator = RunnableExceptionValidator(
+        lambda: jsonschema.validate(failure_configuration_not_object_config, config_json_schema))
+    exception_validator.verify_json_schema_exception(
+        jsonschema.exceptions.ValidationError,
+        "is not of type 'object'",
+        ['monitors', 0, 'notifiers', 'monitor_failure', 'configuration']
+    )
+
+
+
